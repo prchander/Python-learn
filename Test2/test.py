7c7c4c734d2f9415e25cc811a0be9612b3d2ba18
pi <cpranaviyer@gmail.com>
2020-06-16 13:03:26 -0600

    correcting errors

--- Test2/test.py
+++ Test2/test.py
@@ -1,6 +1,10 @@
-from ..Test1 import defined
+import sys
+
+sys.path.insert(1,'/home/pi/Python-learn/Test1')
+
+import Test1
 
 d = 40 
 e = 70
 
-print (defined.add(d,e))
+print (defined.add(40,70))
